051a2d34412f6207457322537157c0b935053de8
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-07-14 17:17:25 +0700

    fix text justify in 'RawStreamTester.h'

--- RawStreamTester.h
+++ RawStreamTester.h
@@ -7,18 +7,18 @@
 //------------------------------------------------------------------------------
 class TStreamTester
 {
-	public:
-		typedef uint32_t TData;
+    public:
+        typedef uint32_t TData;
 
         TStreamTester(TData  increment = 1, TData startValue = 0) : mIncrement(increment), mValue(startValue), mRxBufs(0), mTxBufs(0) {}
-		void fillBuf(uint8_t* buf, unsigned len);
-		unsigned checkBuf(uint8_t* buf, unsigned len, bool printDiff = false);
+        void fillBuf(uint8_t* buf, unsigned len);
+        unsigned checkBuf(uint8_t* buf, unsigned len, bool printDiff = false);
         uint64_t getRxBufs() const { return mRxBufs; }
         uint64_t getTxBufs() const { return mTxBufs; }
 
-	private:
-		const TData mIncrement;
-        TData	 	mValue;
+    private:
+        const TData mIncrement;
+        TData	    mValue;
         uint64_t    mRxBufs;
         uint64_t    mTxBufs;
 };
